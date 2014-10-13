e07e583e86c531ecda55569b7a4fb5fa6885fb26
Babak Loni <babak.loni@gmail.com>
2014-10-13 19:42:05 +0200

    Added positive feedback in datacontainer

--- RecommendationFramework2/DataContainer.cs
+++ RecommendationFramework2/DataContainer.cs
@@ -11,30 +11,20 @@ namespace WrapRec
         public Dictionary<string, User> Users { get; private set; }
         public Dictionary<string, Item> Items { get; private set; }
         public ICollection<ItemRating> Ratings { get; private set; }
+        public ICollection<UserItem> PositiveFeedbacks { get; private set; }
 
         public DataContainer()
         {
             Users = new Dictionary<string, User>();
             Items = new Dictionary<string, Item>();
             Ratings = new HashSet<ItemRating>();
+            PositiveFeedbacks = new HashSet<UserItem>();
         }
 
         public virtual ItemRating AddRating(string userId, string itemId, float rating, bool isTest)
         {
-            User u;
-            Item i;
-
-            if (!Users.TryGetValue(userId, out u))
-            {
-                u = new User(userId);
-                Users.Add(userId, u);
-            }
-
-            if (!Items.TryGetValue(itemId, out i))
-            {
-                i = new Item(itemId);
-                Items.Add(itemId, i);
-            }
+            User u = AddUser(userId);
+            Item i = AddItem(itemId);
 
             var ir = new ItemRating(u, i, rating);
             ir.IsTest = isTest;
@@ -44,6 +34,44 @@ namespace WrapRec
             i.Ratings.Add(ir);
 
             return ir;
+        }
+
+        public virtual UserItem AddPositiveFeedback(string userId, string itemId)
+        {
+            User u = AddUser(userId);
+            Item i = AddItem(itemId);
+
+            var ui = new UserItem(u, i);
+
+            PositiveFeedbacks.Add(ui);
+
+            return ui;
+        }
+
+        public User AddUser(string userId)
+        {
+            User u;
+
+            if (!Users.TryGetValue(userId, out u))
+            {
+                u = new User(userId);
+                Users.Add(userId, u);
+            }
+
+            return u;
+        }
+
+        public Item AddItem(string itemId)
+        {
+            Item i;
+
+            if (!Items.TryGetValue(itemId, out i))
+            {
+                i = new Item(itemId);
+                Items.Add(itemId, i);
+            }
+
+            return i;
         }
 
         public override string ToString()
