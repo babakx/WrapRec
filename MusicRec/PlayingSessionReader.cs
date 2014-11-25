96cbe7ac40fa2d611c2c5e6482f1b0647ee6a261
Babak Loni <babak.loni@gmail.com>
2014-11-25 17:11:24 +0100

    Added the MusicRec demo to the solution together with a basic reader

---
+++ MusicRec/PlayingSessionReader.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WrapRec;
+using WrapRec.Data;
+using System.IO;
+
+namespace MusicRec
+{
+    public class PlayingSessionReader : IDatasetReader
+    {
+        string _path;
+        
+        public PlayingSessionReader(string path)
+        {
+            _path = path;
+        }
+
+        public void LoadData(DataContainer container)
+        {
+            if (!(container is MusicDataContainer))
+            {
+                throw new Exception("The data container should have type MusicDataContainer.");
+            }
+
+            var mContainer = (MusicDataContainer)container;
+
+            foreach (string line in File.ReadAllLines(_path))
+            {
+                var tokens = line.Split(',');
+                var us = new UserSession() { User = mContainer.AddUser(tokens[0]) };
+                var tracks = tokens.Skip(2).Aggregate((a, b) => a + "," + b).Split('|');
+
+                foreach (string track in tracks)
+                {
+                    var parts = track.Split(',');
+                    var item = mContainer.AddItem(parts[0]);
+                    us.Items.Add(item);
+                }
+
+                mContainer.Sessions.Add(us);
+            }
+        }
+    }
+}
---
+++ MusicRec/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicRec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"D:\Data\Datasets\Last.fm\babak_user sessions.csv";
+
+            var reader = new PlayingSessionReader(path);
+            var container = new MusicDataContainer();
+
+            reader.LoadData(container);
+
+            Console.WriteLine("Finished!");
+            Console.Read();
+        }
+    }
+}
