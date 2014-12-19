261f7633c03bd445e7f7f6c7f4c1e168d3846e1b
Babak Loni <babak.loni@gmail.com>
2014-12-19 16:59:11 +0100

    Introduced a new way of building aux. feature vectors based on taking the AuxRating from sum all domains

--- RecommendationFramework2/CrossDomainLibFmFeatureBuilder.cs
+++ RecommendationFramework2/CrossDomainLibFmFeatureBuilder.cs
@@ -75,29 +75,82 @@ namespace WrapRec
             Func<ItemRating, bool> checkActivation = r => r.Domain.IsActive == true; //r.IsActive == true;
 
             var domainRatings = user.Ratings.Where(checkActivation).GroupBy(r => r.Domain.Id);
+            int userAuxDomains = user.Ratings.Select(r => r.Domain.Id).Distinct().Where(d => d != TargetDomain.Id).Count();
+
+            int perDomainRatings;
+            if (userAuxDomains > 0)
+                perDomainRatings = (int)Math.Ceiling((double)NumAuxRatings / userAuxDomains);
+            else
+                perDomainRatings = 0;
+
             var avgUserRating = user.Ratings.Average(r => r.Rating);
+
+            int take = perDomainRatings;
+            int remain = NumAuxRatings;
 
             foreach (var d in domainRatings)
             {
                 if (d.Key != TargetDomain.Id)
                 { 
                     int ratingCount = d.Count();
-                    
-                    string domainExtendedVector = d.OrderByDescending(r => r.Item.Ratings.Count)
-                        //d.Shuffle()
-                        .Take(NumAuxRatings)
-                        // ItemIds are concateneated with domain id to make sure that items in different domains are being distingushed
-                        //.Select(dr => string.Format("{0}:1", Mapper.ToInternalID(dr.Item.Id + d.Key.Id)))
-                        .Select(dr => string.Format("{0}:{1:0.0000}", Mapper.ToInternalID(dr.Item.Id), (double) (dr.Rating - avgUserRating) / 4 + 1)) // dr.Rating / ratingCount))
-                        .Aggregate((cur, next) => cur + " " + next);
+                    string domainExtendedVector = "";
+
+                    var ratings = d.OrderByDescending(r => r.Item.Ratings.Count).Take(take);
+
+                    if (ratings.Count() > 0)
+                    {
+                        domainExtendedVector = ratings
+                            .Select(dr => string.Format("{0}:{1:0.0000}", Mapper.ToInternalID(dr.Item.Id), (double)(dr.Rating - avgUserRating) / 4 + 1)) // dr.Rating / ratingCount))
+                            .Aggregate((cur, next) => cur + " " + next);
+                    }
 
                     if (!String.IsNullOrEmpty(domainExtendedVector.TrimEnd(' ')))
                         extendedVector += " " + domainExtendedVector;
-                     
+
+                    remain = remain - perDomainRatings;
+                    if (remain > 0)
+                        take = perDomainRatings;
+                    else
+                        take = 0;
                 }
             }
             
             return extendedVector;
         }
+
+        // build feature vectors based on considering NumAuxRating ratings per domain
+
+        //private string BuildUserCrossDomainFeatureVector(User user)
+        //{
+        //    string extendedVector = "";
+
+        //    Func<ItemRating, bool> checkActivation = r => r.Domain.IsActive == true; //r.IsActive == true;
+
+        //    var domainRatings = user.Ratings.Where(checkActivation).GroupBy(r => r.Domain.Id);
+        //    var avgUserRating = user.Ratings.Average(r => r.Rating);
+
+        //    foreach (var d in domainRatings)
+        //    {
+        //        if (d.Key != TargetDomain.Id)
+        //        {
+        //            int ratingCount = d.Count();
+
+        //            string domainExtendedVector = d.OrderByDescending(r => r.Item.Ratings.Count)
+        //                //d.Shuffle()
+        //                .Take(NumAuxRatings)
+        //                // ItemIds are concateneated with domain id to make sure that items in different domains are being distingushed
+        //                //.Select(dr => string.Format("{0}:1", Mapper.ToInternalID(dr.Item.Id + d.Key.Id)))
+        //                .Select(dr => string.Format("{0}:{1:0.0000}", Mapper.ToInternalID(dr.Item.Id), (double)(dr.Rating - avgUserRating) / 4 + 1)) // dr.Rating / ratingCount))
+        //                .Aggregate((cur, next) => cur + " " + next);
+
+        //            if (!String.IsNullOrEmpty(domainExtendedVector.TrimEnd(' ')))
+        //                extendedVector += " " + domainExtendedVector;
+
+        //        }
+        //    }
+
+        //    return extendedVector;
+        //}
+
     }
 }
