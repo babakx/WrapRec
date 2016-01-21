88479d2049490c70987df7f7daab20361b115d12
Babak Loni <babak.loni@gmail.com>
2016-01-21 19:29:23 +0100

    Added Logger class and configurations for NLog

---
+++ WrapRec/Logger.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WrapRec
+{
+	public static class Logger
+	{
+		// Log levels
+		// Trace, Debug, Info, Warn, Error, Fatal
+		public static NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+	}
+}
