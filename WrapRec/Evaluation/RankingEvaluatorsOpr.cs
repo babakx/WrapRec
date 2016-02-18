244dd98d7a2281b974cf1558d4d6bd03048d355f
Babak Loni <babak.loni@gmail.com>
2016-02-18 15:28:16 +0100

    Introduced userBased Ranking evaluator with new measures (precision, map and diversity)

--- WrapRec/Evaluation/RankingEvaluatorsOpr.cs
+++ WrapRec/Evaluation/RankingEvaluatorsOpr.cs
@@ -13,73 +13,8 @@ using LinqLib.Sequence;
 
 namespace WrapRec.Evaluation
 {
-	public class RankingEvaluatorsOpr : Evaluator
+	public class RankingEvaluatorsOpr : RankingEvaluators
 	{
-        public string CandidateItemsFile { get; private set; }
-        public CandidateItems CandidateItemsMode { get; private set; }
-		public int[] NumCandidates { get; private set; }
-		public int[] CutOffs { get; private set; }
-
-        int _maxNumCandidates;
-        IList<string> _allCandidateItems;
-
-		public override void Setup()
-		{
-            if (!SetupParameters.ContainsKey("candidateItemsMode"))
-                CandidateItemsMode = CandidateItems.TRAINING;
-            else
-                CandidateItemsMode = (CandidateItems)Enum.Parse(typeof(CandidateItems), SetupParameters["candidateItemsMode"], true);
-
-            if (SetupParameters.ContainsKey("candidateItemsFile"))
-                CandidateItemsFile = SetupParameters["candidateItemsFile"];
-            else if (CandidateItemsMode == CandidateItems.EXPLICIT)
-                throw new WrapRecException("Expect a 'candidateItemsFile' for the mode 'explicit!'");
-
-            CutOffs = SetupParameters["cutOffs"].Split(',').Select(c => int.Parse(c)).ToArray();
-
-            if (!SetupParameters.ContainsKey("numCandidates"))
-                NumCandidates = new int[] { int.MaxValue };
-            else
-                NumCandidates = SetupParameters["numCandidates"].Split(',').Select(n =>
-                {
-                    return (n == "max") ? int.MaxValue : int.Parse(n);
-                }).ToArray();
-
-            _maxNumCandidates = NumCandidates.Max();
-		}
-
-        public void InitializeCandidateItems(Split split)
-        {
-            if (CandidateItemsMode == CandidateItems.TRAINING)
-                _allCandidateItems = split.Train.Select(f => f.Item.Id).Distinct().ToList();
-            else if (CandidateItemsMode == CandidateItems.TEST)
-                _allCandidateItems = split.Test.Select(f => f.Item.Id).Distinct().ToList();
-            else if (CandidateItemsMode == CandidateItems.UNION)
-                _allCandidateItems = split.Container.Items.Values.Select(i => i.Id).ToList();
-            else if (CandidateItemsMode == CandidateItems.OVERLAP)
-            {
-                var trainItems = split.Train.Select(f => f.Item.Id).Distinct();
-                var testItems = split.Test.Select(f => f.Item.Id).Distinct();
-
-                _allCandidateItems = trainItems.Intersect(testItems).ToList();
-            }
-            else // explicit
-            {
-                _allCandidateItems = File.ReadAllLines(CandidateItemsFile).ToList();
-            }
-        }
-
-        public IEnumerable<string> GetCandidateItems(Split split, User u)
-        {
-            var userItems = u.Feedbacks.Select(f => f.Item.Id);
-            return _allCandidateItems.Except(userItems).Take(_maxNumCandidates);
-        }
-
-        public IEnumerable<string> GetRelevantItems(Split split, User user)
-        {
-            return user.Feedbacks.Where(f => f.SliceType == FeedbackSlice.TEST)
-                .Select(f => f.Item.Id).Distinct();
-        }
 
         public override void Evaluate(EvaluationContext context, Model model, Split split)
 		{
@@ -149,7 +84,7 @@ namespace WrapRec.Evaluation
                     var results = new Dictionary<string, string>();
                     results.Add("TestedCases", testedCases.ToString());
                     results.Add("CandidatesMode", CandidateItemsMode.ToString());
-					results.Add("NumCandidates", maxCand.ToString());
+					results.Add("NumCandidates", maxCand == int.MaxValue ? "max" : maxCand.ToString());
 					results.Add("CutOff", k.ToString());
 					results.Add("Recall", string.Format("{0:0.0000}", recallsOpr[maxCand, k]));
 					results.Add("MRR", string.Format("{0:0.0000}", mrrOpr[maxCand, k]));
@@ -159,32 +94,7 @@ namespace WrapRec.Evaluation
 					context.AddResultsSet("rankingMeasures", results);
 				}				
 			}
-
 		}
-
-        protected int IndexOfNewItem(IList<Tuple<string, double>> descSortedList, double newItemScore)
-        {
-            int startIndex = 0, endIndex = descSortedList.Count - 1;
-
-            while (startIndex + 1 < endIndex)
-            {
-                int index = (startIndex + endIndex) / 2;
-
-                if (newItemScore == descSortedList[index].Item2)
-                    return index;
-                else if (newItemScore < descSortedList[index].Item2)
-                    startIndex = index;
-                else
-                    endIndex = index;
-            }
-
-            if (newItemScore >= descSortedList[startIndex].Item2)
-                return startIndex;
-            else if (newItemScore < descSortedList[endIndex].Item2)
-                return endIndex + 1;
-            else
-                return endIndex;
-        }
     }
 
 }
