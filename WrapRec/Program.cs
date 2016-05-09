9a3a1b061d70b819a5c059b2b55ba3ce582eb477
Babak Loni <babak.loni@gmail.com>
2016-05-09 14:40:48 +0200

    added option to change current working directory

--- WrapRec/Program.cs
+++ WrapRec/Program.cs
@@ -5,24 +5,38 @@ using System.Text;
 using System.Threading.Tasks;
 using WrapRec.Utils;
 using WrapRec.Core;
+using System.IO;
 
 namespace WrapRec
 {
+
 	class Program
 	{
 		static void Main(string[] args)
 		{
-			string usage = @"WrapRec 2.0 recommendation toolkit. \nUsage: WrapRec.exe [configFile]";
-			if (args.Length != 1)
+			string usage = @"WrapRec 2.0 recommendation toolkit. \nUsage: WrapRec.exe configFile [--cwd=current-working-directory]";
+
+			if (args.Length < 1 || args.Length > 2)
 			{
 				Console.WriteLine(usage);
 				return;
+			}
+			else if (args.Length == 2)
+			{
+				var parts = args[1].Split('=');
+				if (parts[0] != "--cwd")
+				{
+					Console.WriteLine("Invalid arguments!");
+					return;
+				}
+
+				Environment.CurrentDirectory = parts[1];
 			}
 
 			var em = new ExperimentManager(args[0]);
 			em.RunExperiments();
 		}
-
+	
     }
 
 
